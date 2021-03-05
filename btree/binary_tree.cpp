67cf98d9777bce92eb895c0f2bf159ba185e82f8
Populim <matheusspopulim@usp.br>
2021-03-05 16:38:46 -0300

    Improved header files organization

--- btree/binary_tree.cpp
+++ btree/binary_tree.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <string>
-
+#include "binary_tree.h"
 using namespace std;
-
-
-
 
 // how it works:
 // https://runestone.academy/runestone/books/published/cppds/Trees/ParseTree.html
@@ -12,68 +9,61 @@ using namespace std;
 //  
 
 //class Binary Tree
-class BT {
-	private:
-		string key;
-		BT* left;
-		BT* right;
-	public:
-		BT(string rootObj){
-			this->key = rootObj;
-			this->left = NULL;
-			this->right = NULL;
-		}
-		
-
-		void insertLeft(string newNode){
-			if(this->left == NULL){
-				this->left = new BT(newNode);
-			}
-			else{
-				BT* newChild = new BT(newNode)
-				newChild->left = this->left;
-				this->left = newChild;
-			}
-		}
-
-
-		void insertRight(string newNode){
-			if(this->right == NULL){
-				this->right = new BT(newNode);
-			}
-			else{
-				BT* newChild = new BT(newNode);
-				newChild->right = this->right;
-				this->right = newChild;
-			}
-		}
-
-		BT* getLeft(){
-			return this->left;
-		}
-
-		BT* getRight(){
-			return this->right;
-		}
-
-		void setRootVal(string val){
-			this->key = val;
-		}
-
-		string getRootVal(){
-			return this->key;
-		}
+// BT::BT {
+// 	private:
+// 		string key;
+// 		BT* left;
+// 		BT* right;
+// 	public:
+BT::BT(string rootObj){
+	this->key = rootObj;
+	this->left = NULL;
+	this->right = NULL;
 }
+
+
+void BT::insertLeft(string newNode){
+	if(this->left == NULL){
+		this->left = new BT(newNode);
+	}
+	else{
+		BT* newChild = new BT(newNode);
+		newChild->left = this->left;
+		this->left = newChild;
+	}
+}
+
+
+void BT::insertRight(string newNode){
+	if(this->right == NULL){
+		this->right = new BT(newNode);
+	}
+	else{
+		BT* newChild = new BT(newNode);
+		newChild->right = this->right;
+		this->right = newChild;
+	}
+}
+
+BT* BT::getLeft(){
+	return this->left;
+}
+
+BT* BT::getRight(){
+	return this->right;
+}
+
+void BT::setRootVal(string val){
+	this->key = val;
+}
+
+string BT::getRootVal(){
+	return this->key;
+}
+
 
 // TODO:
 // implement a stack
 
 
 //implement the string -> binarytree function
-
-
-int main(){
-	cout << " Hello;World \n";
-	
-
-}
---
+++ btree/binary_tree.h
@@ -0,0 +1,24 @@
+#ifndef BT_H
+#define BT_H
+
+#include <iostream>
+
+using namespace std;
+
+class BT{
+	private:
+		string key;
+		BT* left;
+		BT* right;
+	public:
+		BT(string rootObj);
+		void insertLeft(string newNode);
+		void insertRight(string newNode);
+		BT* getLeft();
+		BT* getRight();
+		void setRootVal(string val);
+		string getRootVal();
+};
+
+
+#endif
